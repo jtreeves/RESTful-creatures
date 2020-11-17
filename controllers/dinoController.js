0312805e9f8e0cfc6b38bde2533991fa903a6019
jtreeves <jackson.t.reeves@gmail.com>
2020-11-17 11:52:38 -0500

    [ejs] Finalize dinosaur portion of lab

--- controllers/dinoController.js
+++ controllers/dinoController.js
@@ -1,10 +1,32 @@
-const dinoRouter = require('express').Router()
 const fs = require('fs')
+const dinoRouter = require('express').Router()
 
 dinoRouter.get('/', (req, res) => {
     const rawDinos = fs.readFileSync('./dinosaurs.json')
     const dinos = JSON.parse(rawDinos)
     res.render('dinosaurs/index', { dinos })
+})
+
+// NEW must go above SHOW
+dinoRouter.get('/new', (req, res) => {
+    res.render('dinosaurs/new')
+})
+
+dinoRouter.get('/:id', (req, res) => {
+    const rawDinos = fs.readFileSync('./dinosaurs.json')
+    const dinos = JSON.parse(rawDinos)
+    const id = parseInt(req.params.id) - 1
+    const dino = dinos[id]
+    res.render('dinosaurs/show', { dino })
+})
+
+dinoRouter.post('/', (req, res) => {
+    const newDino = req.body
+    const rawDinos = fs.readFileSync('./dinosaurs.json')
+    const dinos = JSON.parse(rawDinos)
+    dinos.push(newDino)
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinos))
+    res.redirect('/dinosaurs')
 })
 
 module.exports = dinoRouter
--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 // Import Modules
-const app = require('express')()
+const express = require('express')
+const app = express()
 const layouts = require('express-ejs-layouts') // 3rd party imports (should go first in a block)
 
 const dinoRouter = require('./controllers/dinoController') // Your imports (should go as a second block after the first one)
 
 // Set Up as EJS
 app.set('view engine', 'ejs')
+app.use(layouts)
+app.use(express.urlencoded({extended: false}))
 
 // Establish Routes
 app.get('/', (req, res) => {
     res.send('hello, I am alive!')
 })
 
-app.use(layouts)
 app.use('/dinosaurs', dinoRouter)
 
 // Listen
