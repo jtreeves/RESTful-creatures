0620c1395e87b2ee12c1d8405988c0a6f9afbfcc
jtreeves <jackson.t.reeves@gmail.com>
2020-11-17 11:15:52 -0500

    [json] Use JSON as a quasi-database

--- controllers/dinoController.js
+++ controllers/dinoController.js
@@ -1,7 +1,10 @@
 const dinoRouter = require('express').Router()
+const fs = require('fs')
 
 dinoRouter.get('/', (req, res) => {
-    res.render('dinosaurs/index')
+    const rawDinos = fs.readFileSync('./dinosaurs.json')
+    const dinos = JSON.parse(rawDinos)
+    res.render('dinosaurs/index', { dinos })
 })
 
 module.exports = dinoRouter
